8aec8f246fff12d631975b538d6f3943fd3dd27e
Vlad <vlad24urzhumov@gmail.com>
2019-11-12 22:04:54 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// a = 5;
+// console.log(a);
+
+//  console.log(4/0);
+
+
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// console.log(persone["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[0])
+
+
+// alert("hello world");
+
+// let answer = +prompt("Do you have 18?", "Yes, I do");
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let ischecked = false,
+    isClose = false;
+console.log(ischecked || !isClose);
+
